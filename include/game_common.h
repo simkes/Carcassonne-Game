61fc4af8786a0f1c4a4222f6e571da8ca20c6f54
simkes <simksa03@gmail.com>
2022-03-12 10:13:21 -0500

    new tile type

--- include/game_common.h
+++ include/game_common.h
@@ -5,9 +5,9 @@ namespace game_model {
 
 const int CARD_DIMENSION = 5;
 
-enum class Type {NOTHING, CASTLE, FIELD, ROAD, MONASTERY, JUNCTION};
-std::vector <Type> typesVector {Type::NOTHING, Type::CASTLE, Type::FIELD, Type::ROAD, Type::MONASTERY, Type::JUNCTION};
-                                //      0           1           2           3               4               5
+enum class Type {NOTHING, CASTLE, FIELD, ROAD, MONASTERY, JUNCTION, CASTLEWITHSHILD};
+std::vector <Type> typesVector {Type::NOTHING, Type::CASTLE, Type::FIELD, Type::ROAD, Type::MONASTERY, Type::JUNCTION, Type::CASTLEWITHSHILD};
+                                //      0           1           2           3               4               5                   6
 }  // namespace game_model
 
 #endif  // GAME_COMMON_H_
