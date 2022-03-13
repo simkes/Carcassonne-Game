8cc0bf114607d043926f90bc3b919f6032733865
simkes <simksa03@gmail.com>
2022-03-13 06:50:58 -0400

    shield :)

--- include/game_common.h
+++ include/game_common.h
@@ -6,7 +6,7 @@ namespace game_model {
 const int CARD_DIMENSION = 5;
 
 enum class Type {NOTHING, CASTLE, FIELD, ROAD, MONASTERY, JUNCTION, CASTLEWITHSHILD};
-std::vector <Type> typesVector {Type::NOTHING, Type::CASTLE, Type::FIELD, Type::ROAD, Type::MONASTERY, Type::JUNCTION, Type::CASTLEWITHSHILD};
+std::vector <Type> typesVector {Type::NOTHING, Type::CASTLE, Type::FIELD, Type::ROAD, Type::MONASTERY, Type::JUNCTION, Type::CASTLEWITHSHIELD};
                                 //      0           1           2           3               4               5                   6
 }  // namespace game_model
 
