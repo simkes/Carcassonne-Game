53ed346621eda0365d9b8d4df49c963dca069dd8
dezd4r <numkinvs03@gmail.com>
2022-03-21 16:42:55 +0300

    add rotate

--- include/interaction.h
+++ include/interaction.h
@@ -1,49 +1,58 @@
 #ifndef INTERACTION_H_
 #define INTERACTION_H_
 
-#include "game_view.h"
 #include "game_board.h"
 #include "game_card.h"
+#include "game_view.h"
 
 namespace interaction {
 
 enum class State { CARDPLACEMENT, UNITPLACEMENT, DEFAULT };
 
-struct defaultInteraction{
-    explicit defaultInteraction(game_model::Board &gameBoard_, game_view::BoardView &mainView_) : gameBoard(gameBoard_), mainView(mainView_) {};
+struct defaultInteraction {
+    explicit defaultInteraction(game_model::Board &gameBoard_,
+                                game_view::BoardView &mainView_)
+        : gameBoard(gameBoard_), mainView(mainView_){};
     virtual void handleEvent(sf::Event &event, bool &endOfState);
-    //virtual destructor??
+    // virtual destructor??
     virtual ~defaultInteraction() = default;
 
 protected:
     game_view::BoardView mainView;
     game_model::Board gameBoard;
-}; // may be better to name baseInteraction or smt
+};  // may be better to name baseInteraction or smt
 
-struct cardPlacementInteraction: public defaultInteraction{
-
-    explicit cardPlacementInteraction(game_model::Board gameBoard_, game_view::BoardView &mainView_, game_model::Card *currentCard_)
-        : defaultInteraction(gameBoard_, mainView_), currentCard(currentCard_) {}
+struct cardPlacementInteraction : public defaultInteraction {
+    explicit cardPlacementInteraction(game_model::Board gameBoard_,
+                                      game_view::BoardView &mainView_,
+                                      game_model::Card *currentCard_)
+        : defaultInteraction(gameBoard_, mainView_), currentCard(currentCard_) {
+    }
     void handleEvent(sf::Event &event, bool &endOfState) override;
 
 private:
-     //but its so strange, because in BoardView we have board, maybe make getter
+    // but its so strange, because in BoardView we have board, maybe make getter
     game_model::Card *currentCard;
 };
 
-struct unitPlacementInteraction: public defaultInteraction{
-
-    explicit unitPlacementInteraction( game_model::Board gameBoard_,  game_view::BoardView &mainView_,
-                                      game_model::Card *currentCard_, game_model::Player *currentPlayer_)
-        : defaultInteraction(gameBoard_, mainView_), currentCard(currentCard_), currentPlayer(currentPlayer_) {}
+struct unitPlacementInteraction : public defaultInteraction {
+    explicit unitPlacementInteraction(game_model::Board gameBoard_,
+                                      game_view::BoardView &mainView_,
+                                      game_model::Card *currentCard_,
+                                      game_model::Player *currentPlayer_)
+        : defaultInteraction(gameBoard_, mainView_),
+          currentCard(currentCard_),
+          currentPlayer(currentPlayer_) {
+    }
     void handleEvent(sf::Event &event, bool &endOfState) override;
-    //cardPlacementInteraction(game_model::Board &board, game_model::Card &card): Board(board), currentCardPtr(card){}
+    // cardPlacementInteraction(game_model::Board &board, game_model::Card
+    // &card): Board(board), currentCardPtr(card){}
 
 private:
     game_model::Card *currentCard;
     game_model::Player *currentPlayer;
 };
 
-} // namespace interaction
+}  // namespace interaction
 
-#endif // INTERACTION_H_
+#endif  // INTERACTION_H_
--- src/interaction.cpp
+++ src/interaction.cpp
@@ -1,124 +1,135 @@
 #include "interaction.h"
 
-namespace interaction{
+namespace interaction {
 
 void defaultInteraction::handleEvent(sf::Event &event, bool &endOfState) {
-    if (event.type == sf::Event::KeyPressed){
-
-        if (event.key.code == sf::Keyboard::Left){
+    if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Left) {
             mainView.getView().move(-10, 0);
         }
 
-        if (event.key.code == sf::Keyboard::Up){
+        if (event.key.code == sf::Keyboard::Up) {
             mainView.getView().move(0, 10);
         }
 
-        if (event.key.code == sf::Keyboard::Down){
+        if (event.key.code == sf::Keyboard::Down) {
             mainView.getView().move(0, -10);
         }
 
-        if (event.key.code == sf::Keyboard::Right){
+        if (event.key.code == sf::Keyboard::Right) {
             mainView.getView().move(10, 0);
         }
     }
 
-    if (event.type == sf::Event::MouseWheelScrolled){
-
-        if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel){
-            mainView.getView().zoom(static_cast<float>(1.0 + 0.1 * event.mouseWheelScroll.delta));
+    if (event.type == sf::Event::MouseWheelScrolled) {
+        if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel) {
+            mainView.getView().zoom(
+                static_cast<float>(1.0 + 0.1 * event.mouseWheelScroll.delta));
         }
-
     }
-    //endOfState = true; ???
+    // endOfState = true; ???
 }
 
 void cardPlacementInteraction::handleEvent(sf::Event &event, bool &endOfState) {
-
-    if (event.type == sf::Event::KeyPressed){
-
-        if (event.key.code == sf::Keyboard::Left){
+    if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Left) {
             mainView.getView().move(-10, 0);
         }
 
-        if (event.key.code == sf::Keyboard::Up){
+        if (event.key.code == sf::Keyboard::Up) {
             mainView.getView().move(0, 10);
         }
 
-        if (event.key.code == sf::Keyboard::Down){
+        if (event.key.code == sf::Keyboard::Down) {
             mainView.getView().move(0, -10);
         }
 
-        if (event.key.code == sf::Keyboard::Right){
+        if (event.key.code == sf::Keyboard::Right) {
             mainView.getView().move(10, 0);
         }
-    }
 
-    if (event.type == sf::Event::MouseWheelScrolled){
-
-        if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel){
-            mainView.getView().zoom(static_cast<float>(1.0 + 0.1 * event.mouseWheelScroll.delta));
+        if (event.key.code == sf::Keyboard::Q) {
+            currentCard->rotateLeft();
         }
 
+        if (event.key.code == sf::Keyboard::E) {
+            currentCard->rotateRight();
+        }
     }
 
-    if (event.type == sf::Event::MouseButtonPressed){
-        if (event.mouseButton.button == sf::Mouse::Left){
-            sf::Vector2i clickCoordinates(mainView.getView().getCenter());
-            clickCoordinates.x += (event.mouseButton.x - static_cast<int>(mainView.getView().getSize().x/2));
-            clickCoordinates.y += (event.mouseButton.y - static_cast<int>(mainView.getView().getSize().y/2));//TODO: make coordinate convert easier
+    if (event.type == sf::Event::MouseWheelScrolled) {
+        if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel) {
+            mainView.getView().zoom(
+                static_cast<float>(1.0 + 0.1 * event.mouseWheelScroll.delta));
+        }
+    }
 
-            if (gameBoard.canAddCard(clickCoordinates, *currentCard)){
+    if (event.type == sf::Event::MouseButtonPressed) {
+        if (event.mouseButton.button == sf::Mouse::Left) {
+            sf::Vector2i clickCoordinates(mainView.getView().getCenter());
+            clickCoordinates.x +=
+                (event.mouseButton.x -
+                 static_cast<int>(mainView.getView().getSize().x / 2));
+            clickCoordinates.y +=
+                (event.mouseButton.y -
+                 static_cast<int>(mainView.getView().getSize().y /
+                                  2));  // TODO: make coordinate convert easier
+
+            if (gameBoard.canAddCard(clickCoordinates, *currentCard)) {
                 gameBoard.addCard(clickCoordinates, *currentCard);
                 endOfState = true;
-            } // what to do with else: error? warning? nothing?
+            }  // what to do with else: error? warning? nothing?
         }
     }
 }
 
 void unitPlacementInteraction::handleEvent(sf::Event &event, bool &endOfState) {
-    if (event.type == sf::Event::KeyPressed){
-
-        if (event.key.code == sf::Keyboard::Enter){
+    if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Enter) {
             endOfState = true;
             return;
         }
 
-        if (event.key.code == sf::Keyboard::Left){
+        if (event.key.code == sf::Keyboard::Left) {
             mainView.getView().move(-10, 0);
         }
 
-        if (event.key.code == sf::Keyboard::Up){
+        if (event.key.code == sf::Keyboard::Up) {
             mainView.getView().move(0, 10);
         }
 
-        if (event.key.code == sf::Keyboard::Down){
+        if (event.key.code == sf::Keyboard::Down) {
             mainView.getView().move(0, -10);
         }
 
-        if (event.key.code == sf::Keyboard::Right){
+        if (event.key.code == sf::Keyboard::Right) {
             mainView.getView().move(10, 0);
         }
     }
 
-    if (event.type == sf::Event::MouseWheelScrolled){
-
-        if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel){
-            mainView.getView().zoom(static_cast<float>(1.0 + 0.1 * event.mouseWheelScroll.delta));
+    if (event.type == sf::Event::MouseWheelScrolled) {
+        if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel) {
+            mainView.getView().zoom(
+                static_cast<float>(1.0 + 0.1 * event.mouseWheelScroll.delta));
         }
-
     }
 
-    if (event.type == sf::Event::MouseButtonPressed){
-        if (event.mouseButton.button == sf::Mouse::Left){
+    if (event.type == sf::Event::MouseButtonPressed) {
+        if (event.mouseButton.button == sf::Mouse::Left) {
             sf::Vector2i clickCoordinates(mainView.getView().getCenter());
-            clickCoordinates.x += (event.mouseButton.x - static_cast<int>(mainView.getView().getSize().x/2));
-            clickCoordinates.y += (event.mouseButton.y - static_cast<int>(mainView.getView().getSize().y/2));//TODO: make coordinate convert easier
+            clickCoordinates.x +=
+                (event.mouseButton.x -
+                 static_cast<int>(mainView.getView().getSize().x / 2));
+            clickCoordinates.y +=
+                (event.mouseButton.y -
+                 static_cast<int>(mainView.getView().getSize().y / 2));
 
-
-            gameBoard.getTiles()[clickCoordinates].unit = currentPlayer->get_unit();
-            gameBoard.getTiles()[clickCoordinates].unit->tile = &gameBoard.getTiles()[clickCoordinates];
+            gameBoard.getTiles()[clickCoordinates].unit =
+                currentPlayer->get_unit();
+            gameBoard.getTiles()[clickCoordinates].unit->tile =
+                &gameBoard.getTiles()[clickCoordinates];
         }
     }
 }
 
-}
+}  // namespace interaction
