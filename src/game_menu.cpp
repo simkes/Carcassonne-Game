58f1af451593e040f74256ed4b5275a3efb81ef3
dezd4r <numkinvs03@gmail.com>
2022-06-05 04:24:39 +0300

    fixed crash in menu

--- src/game_menu.cpp
+++ src/game_menu.cpp
@@ -296,8 +296,10 @@ std::pair<sf::String, int> Menu::ask_name_color( const std::vector<int>& availab
                     }
                 } else if (event.type == sf::Event::KeyPressed) {
                     if (event.key.code == sf::Keyboard::BackSpace) {
-                        stringEntered.erase(stringEntered.getSize() - 1);
-                        textEntered.setString(stringEntered);
+                        if (!stringEntered.isEmpty()){
+                            stringEntered.erase(stringEntered.getSize() - 1);
+                            textEntered.setString(stringEntered);
+                        }
                     } else if (event.key.code == sf::Keyboard::Enter) {
                         if (stringEntered == "") {
                             wrongN = true;
